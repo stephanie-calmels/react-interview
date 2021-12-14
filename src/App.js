fdebd0414fea1f84cbff258b4cedc5b21853981b
stephanie-calmels <stephcalmels82@gmail.com>
2021-12-14 13:22:22 +0100

    add: paging

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -15,25 +15,46 @@ function App({ getMovies, movies, filteredMovies }) {
   // eslint-disable-next-line
   []);
 
+  const [itemsPerPage, setItemsPerPage] = useState(4);
+  const [itemOffset, setItemOffset] = useState(0);
+  let currentMovies = [];
+
+  const endOffset = itemOffset + itemsPerPage;
+  if (filteredMovies.length > 0) {
+    currentMovies = filteredMovies.slice(itemOffset, endOffset);
+  } else if (movies.length > 0 ) {
+    currentMovies = movies.slice(itemOffset, endOffset);
+  }
+
+  const handleClick = (direction) => { 
+    let newOffset;
+    if (direction === 'previous') {
+      newOffset = itemOffset - itemsPerPage;
+      if (newOffset < 0) {
+        setItemOffset(0);
+      } else {
+        setItemOffset(newOffset);
+      }
+    }
+
+    if (direction === 'next') {
+      newOffset = itemOffset + itemsPerPage;
+      if (newOffset > movies.length) {
+        setItemOffset(itemOffset);
+      } else {
+        setItemOffset(newOffset);
+      }
+    }
+
+  }
+
   return (
     <div className="App">
       <Categories />
       <CardContainer>
-        { 
-          filteredMovies.length > 0 ?
-            filteredMovies.map(movie => (
-              <Card
-                key={movie.id}
-                title={movie.title}
-                category={movie.category}
-                likes={movie.likes}
-                dislikes={movie.dislikes}
-                id={movie.id}
-              />   
-            )) 
-          :
-          movies.length > 0 && 
-          movies.map(movie => (
+        {
+          currentMovies.length > 0 && 
+          currentMovies.map(movie => (
             <Card
               key={movie.id}
               title={movie.title}
@@ -43,8 +64,63 @@ function App({ getMovies, movies, filteredMovies }) {
               id={movie.id}
             />   
           )) 
+
         }
       </CardContainer>
+
+      <div className='paging'>
+
+        <div className='buttons'>
+          <button 
+            className='button'
+            onClick={() => handleClick('previous')}
+          >
+            Précédent
+          </button>
+
+          <button 
+            className='button'
+            onClick={() => handleClick('next')}
+          >
+            Suivant
+          </button>
+        </div>
+
+        <div className='items'>
+          <p>Nombre d'éléments par page :</p>
+          <div>
+              <input 
+                type="radio" 
+                id='4' 
+                name='item' 
+                value={4}
+                defaultChecked
+                onChange={() => setItemsPerPage(4)}
+                />
+              <label htmlFor='4'>4</label> 
+
+              <input 
+                type="radio" 
+                id='8' 
+                name='item' 
+                value={8}
+                onChange={() => setItemsPerPage(8)}
+                />
+              <label htmlFor='8'>8</label>
+
+              <input 
+                type="radio" 
+                id='12'
+                name='item'
+                value={12}
+                onChange={() => setItemsPerPage(12)}
+                />
+              <label htmlFor='12'>12</label>
+          </div>
+        </div>
+
+        </div>
+
     </div>
   );
 }
